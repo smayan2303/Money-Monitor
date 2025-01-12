aa82122c65c8427b455c1268a61c03600a4061c8
Smayan Ranjan <smayanranjan2303@gmail.com>
2025-01-11 21:14:53 -0500

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
