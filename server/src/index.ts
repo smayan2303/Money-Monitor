100d6a94f5d8ae7927d6bee27e2d7f5fead08223
Smayan Ranjan <smayanranjan2303@gmail.com>
2025-01-11 20:53:21 -0500

    "Fixed backend environment variables"

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Loads environment variables from the .env file
+
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 
-const mongoURI: string =
-"mongodb+srv://money:monitor@personalfinancetracker.fzzwx.mongodb.net/";
+
+const mongoURI: string | undefined = process.env.MONGODB_URL;
+
+if (!mongoURI) {
+  throw new Error("MONGODB_URL is not defined in the environment variables");
+}
 
 mongoose.connect(mongoURI).then(() => 
     console.log("CONNECTED TO MONGODB")
@@ -22,3 +29,4 @@ app.use("/financial-records", financialRecordRouter);
 app.listen(port, () => {
     console.log(`Server Running on Port ${port}`);
   });
+
